243391972adc98eddf584390cc96ed0f06a59379
Jen E <jenevans3ten@gmail.com>
2020-10-29 10:46:15 -0400

    abstracted code into separate functions; refactored to class structure

--- src/js/app.js
+++ src/js/app.js
@@ -1,25 +1,67 @@
-const getFizzBuzzValue = function(number){
-    if(number % 3 === 0 && number % 5 === 0){
-        return "FizzBuzz"
-    }
-    else if(number % 3 === 0){
-        return "Fizz"
-    }
-    else if(number % 5 === 0){
-        return "Buzz"
+class FizzBuzz{
+    // constructor
+    constructor(){
+
     }
 
-    return number.toString();
+    // functions in a class are Methods
+    getFizzBuzzValue(number){
+        if(this.isDivisibleBy3(number) && this.isDivisibleBy5(number)){
+            return "FizzBuzz"
+        }
+        else if(this.isDivisibleBy3(number)){
+            return "Fizz"
+        }
+        else if(this.isDivisibleBy5(number)){
+            return "Buzz"
+        }
+    
+        return number.toString();
+    }
+
+    isDivisibleBy3(number){
+        return number % 3 === 0
+    }
+
+    isDivisibleBy5(number){
+        return number % 5 === 0
+    }
+
 }
+
+let fizzBuzz = new FizzBuzz()
 
 let playingGame = true
 let numberInput
 let result
 do{
     numberInput = prompt('Enter a number')
-    result = getFizzBuzzValue(numberInput)
+    result = fizzBuzz.getFizzBuzzValue(numberInput)
     alert(result)
     playingGame = confirm('Do you want to play again?')
 }while(playingGame)
 
 alert('Good bye!')
+
+// const getFizzBuzzValue = function(number){
+//     if(isDivisibleBy3(number) && isDivisibleBy5(number)){
+//         return "FizzBuzz"
+//     }
+//     else if(isDivisibleBy3(number)){
+//         return "Fizz"
+//     }
+//     else if(isDivisibleBy5(number)){
+//         return "Buzz"
+//     }
+
+//     return number.toString();
+// }
+
+// function isDivisibleBy3(number){
+//     return number % 3 === 0
+// }
+
+// function isDivisibleBy5(number){
+//     return number % 5 === 0
+// }
+
